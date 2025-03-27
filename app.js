99c9ee08ba89d7201525098683277774b84c037a
Zanzanzanan <mznpingol@tip.edu.ph>
2025-03-27 13:33:33 +0800

    Fixing render

--- app.js
+++ app.js
@@ -59,8 +59,10 @@ app.use(function(err, req, res, next) {
 
 // Start Server
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server started on port ${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
 module.exports = app;
